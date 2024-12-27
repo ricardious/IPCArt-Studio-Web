832d846c08df9bd5b440c977f7a920562dd07636
ricardious <castaneda.systems@gmail.com>
2024-12-26 20:23:37 -0600

    feat(user-model): add User class to handle user details

---
+++ backend/models/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(
+        self, user_id, pwd, full_name, email, phone_number, address, profile_url
+    ):
+        self.user_id = user_id
+        self.pwd = pwd
+        self.full_name = full_name
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+        self.profile_url = profile_url
