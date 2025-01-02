8c3bd9571c586f6fcd88508208cd238dc13b6786
ricardious <castaneda.systems@gmail.com>
2025-01-01 23:57:31 -0600

    feat: add new URL patterns for image upload, editor, and help views

--- frontend/apps/images/urls.py
+++ frontend/apps/images/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import gallery
+from .views import gallery, upload, image_editor, help_view
 
 urlpatterns = [
     path("user/gallery/", gallery, name="gallery"),
+    path("user/upload_image/", upload, name="upload_image"),
+    path("user/image_editor/", image_editor, name="image_editor"),
+    path("user/help/", help_view, name="help"),
 ]
